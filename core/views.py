0ba014c2397f8fe705c3fdd6f1d8bb37e5f7d448
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-24 23:49:16 -0300

    Cutomize JWT

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CloudTokenObtainPairSerializer
+
 
 # Create your views here.
+class CloudTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CloudTokenObtainPairSerializer
