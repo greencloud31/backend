bb9dcecfe9a99af4a1bd7f76ce41563127079d19
Aditya Deepak Mahale <ad619659@dal.ca>
2022-03-30 20:26:38 -0300

    Add convert endpoint

--- audiotranslator/urls.py
+++ audiotranslator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.root),
+    path("convert/", views.convert),
 ]
--- audiotranslator/views.py
+++ audiotranslator/views.py
@@ -1,6 +1,30 @@
+import json
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.views import Response
+from rest_framework import status
 
 # Create your views here.
 def root(request):
     return HttpResponse("ok")
+
+
+@api_view(["POST"])
+def convert(request):
+    body = json.loads(request.body.decode("utf-8"))
+    if "text" not in body or "language" not in body:
+        return Response(
+            {
+                "error": [
+                    "Missing required fields text, language."
+                ]
+            },
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    return Response(
+            {
+                "text": body["text"],
+                "language": body["language"]
+            },
+            status=status.HTTP_200_OK,
+        )
